1d60a1f2b00add8c238861563e7a5969bfabdfd7
JoelMolina <joelmolinavelez21@gmail.com>
2023-11-26 00:01:09 -0500

    Subida del proyecto

---
+++ src/main/java/com/cursos/api/springsecuritycourse/SpringSecurityCourseApplication.java
@@ -0,0 +1,13 @@
+package com.cursos.api.springsecuritycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursos/api/springsecuritycourse/SpringSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursos.api.springsecuritycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
