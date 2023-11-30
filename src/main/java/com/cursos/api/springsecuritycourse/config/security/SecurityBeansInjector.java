122f23f75d29f8c9903c10b1894f3e7bd3a1a05d
JoelMolina <joelmolinavelez21@gmail.com>
2023-11-30 14:02:30 -0500

    Se crea la estrategia de authenticación mediante UserDetailsSeervice y PasswordEncode

--- src/main/java/com/cursos/api/springsecuritycourse/config/security/SecurityBeansInjector.java
+++ src/main/java/com/cursos/api/springsecuritycourse/config/security/SecurityBeansInjector.java
@@ -1,14 +1,23 @@
 package com.cursos.api.springsecuritycourse.config.security;
 
+import com.cursos.api.springsecuritycourse.exceptions.ObjectNotFoundException;
+import com.cursos.api.springsecuritycourse.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityBeansInjector {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -17,8 +26,19 @@ public class SecurityBeansInjector {
     @Bean //Strategias  de authenticación
     public AuthenticationProvider authenticationProvider (){
         DaoAuthenticationProvider authenticationStrategy = new DaoAuthenticationProvider(); //recibe el UserDatailService y la contraseña sin encriptar y mediante PassWord Encoder se la encripta
-        authenticationStrategy.setPasswordEncoder(null);
-        authenticationStrategy.setUserDetailsService(null);
+        authenticationStrategy.setPasswordEncoder(passwordEncoder());
+        authenticationStrategy.setUserDetailsService(userDetailsService());
         return authenticationStrategy;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){ // CODIFICAR CONTRASEÑA QUE VIENE
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return (username) -> { // se crea una función la lambda para así poder utilizar el método de UserDetailsService ya que solo tiene 1
+            return userRepository.findByUsername(username)
+                    .orElseThrow(() -> new ObjectNotFoundException("User not found with username " + username));
+        };
     }
 }
